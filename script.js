738fedaca9a0bd91bda10b7e269db686f9124d8e
asimtimsina <timsinaasim@gmail.com>
2023-06-13 14:56:43 -0500

    //170. Nesting Conditionals

--- script.js
+++ script.js
@@ -39,18 +39,35 @@ else if (dayOfWeek === 'Friday') {
 // 10-65 - Adult $20
 // 65+ - Senior $10
 
-let age = 8;
+// let age = 8;
 
-if (age < 5) {
-    console.log("You are a baby. You get in for free!")
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!")
+// }
+// else if (age < 10) {
+//     console.log("You are a child. You pay $10!")
+// }
+// else if (age < 65) {
+//     console.log("You are an adult. You pay $20!")
+// }
+// //169. Else
+// else {
+//     console.log("You are a senior. You pay $10!")
+// }
+
+
+//170. Nesting Conditionals
+const password = prompt("Please enter a new password");
+
+if (password.length >= 6) {
+    // console.log("Long enough password!")
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!")
+    }
+    else {
+        console.log("Password cannot contain spaces!")
+    }
+} else {
+    console.log("Password too short! Must be 6+ characters")
 }
-else if (age < 10) {
-    console.log("You are a child. You pay $10!")
-}
-else if (age < 65) {
-    console.log("You are an adult. You pay $20!")
-}
-//169. Else
-else {
-    console.log("You are a senior. You pay $10!")
-}
+
