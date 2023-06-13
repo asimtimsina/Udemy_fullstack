81fb025101ca7ce5d802f53f263256f28af03769
asimtimsina <timsinaasim@gmail.com>
2023-06-13 14:36:57 -0500

    167. If Statements

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ console.log("Hello World!")
 
 let total = 1 + 3;
 console.log(total);
+
+//167. If Statements
+
+
+if (1 + 1 === 2) {
+    console.log("Math still works");
+}
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Your number is less than 0.5");
+    console.log(random);
+}
