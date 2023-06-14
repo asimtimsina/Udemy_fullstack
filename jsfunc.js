2dfa6ebc8ce8798ee170b4c6ae0eaaeb28de324a
asimtimsina <timsinaasim@gmail.com>
2023-06-14 10:47:43 -0500

    208. Our Very First Function

---
+++ jsfunc.js
@@ -0,0 +1,16 @@
+// 207. Intro to Functions
+
+// 208. Our Very First Function
+
+// 2 step Process
+// Define and run
+
+// function funcName(){
+//     //do something
+// }
+
+function singSong() {
+    console.log("TX");
+    console.log("CO");
+    console.log("OK");
+}
