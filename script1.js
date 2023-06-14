d3455f968b03181841503d9a023999ce8816c16f
asimtimsina <timsinaasim@gmail.com>
2023-06-13 19:47:29 -0500

    //188. Creating Object Literals

--- script1.js
+++ script1.js
@@ -2,3 +2,17 @@
 // objects are collections of properties
 // properties are a key value pair
 
+//188. Creating Object Literals
+let person = {
+    firstName: 'Mick',
+    lastName: 'Jagger'
+};
+
+const kitchenSink = {
+    favNum: 123112,
+    isFunny: true,
+    colors: ['red', 'orange'],
+};
+
+
+
