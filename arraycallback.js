6df1dd016d86a75d1a30d6ff87b333b5403b06ff
asimtimsina <timsinaasim@gmail.com>
2023-06-14 14:50:01 -0500

    225. The map Method

--- arraycallback.js
+++ arraycallback.js
@@ -1,16 +1,16 @@
-// 224. The forEach Method
+// // 224. The forEach Method
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function print(element) {
-    console.log(element);
-}
+// function print(element) {
+//     console.log(element);
+// }
 
-numbers.forEach(print);
+// numbers.forEach(print);
 
-numbers.forEach(function (number) {
-    console.log(number);
-})
+// numbers.forEach(function (number) {
+//     console.log(number);
+// })
 
 const movies = [
     {
@@ -24,6 +24,25 @@ const movies = [
 ]
 
 
-movies.forEach(function (movie) {
-    console.log(`${movie.name} rating is ${movie.rating}/100.`)
+// movies.forEach(function (movie) {
+//     console.log(`${movie.name} rating is ${movie.rating}/100.`)
+// })
+
+
+/////////////////////////
+// 225. The map Method
+// map creates a new array from the return value
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const doublenum = numbers.map(function (num) {
+    return num * 2;
 })
+
+console.log(doublenum);
+
+const moviesName = movies.map(function (movie) {
+    return movie.name.toUpperCase();
+})
+
+console.log(moviesName);
