7cf0a9eb0394bd06b33eb79bd2d2ea2dc40d97de
asimtimsina <timsinaasim@gmail.com>
2023-06-14 23:32:58 -0500

    249. innerHTML, textContent, & innerText

--- domjs.js
+++ domjs.js
@@ -31,20 +31,31 @@
 ////////////////////////////////////////////
 //248. querySelector & querySelectorAll
 
-document.querySelector('p') // returns the first element that matches the selector
-document.querySelectorAll('p') // returns a NodeList of all the elements that match the selector
+// document.querySelector('p') // returns the first element that matches the selector
+// document.querySelectorAll('p') // returns a NodeList of all the elements that match the selector
 
-document.querySelector('#banner') // returns the first element with the id banner
-document.querySelector('.paragraph') // returns the first element with the class paragraph
-document.querySelectorAll('.paragraph') // returns a NodeList of all the elements with the class paragraph
+// document.querySelector('#banner') // returns the first element with the id banner
+// document.querySelector('.paragraph') // returns the first element with the class paragraph
+// document.querySelectorAll('.paragraph') // returns a NodeList of all the elements with the class paragraph
 
 
-console.log(document.querySelector("a[title='Java']"))
-console.log(document.querySelectorAll("p"))
-console.log(document.querySelectorAll("p a"))
+// console.log(document.querySelector("a[title='Java']"))
+// console.log(document.querySelectorAll("p"))
+// console.log(document.querySelectorAll("p a"))
 
-const links = document.querySelectorAll("p a")
+// const links = document.querySelectorAll("p a")
 
-for (let each in links) {
-    console.log(each);
-}
+// for (let each in links) {
+//     console.log(each);
+// }
+
+///////////////////////////////
+// 249. innerHTML, textContent, & innerText
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+
+document.querySelector('h2').innerHTML = '<i> Asim </i>' // '<i> Asim </i>
+document.querySelector('h2').innerHTML += '<sub> Timsina </sub>' // '<i> Asim </i><sub> Timsina </sub>'
+
