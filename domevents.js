b698c6fcaed2d1455f1942ead60e6470cc24624b
asimtimsina <timsinaasim@gmail.com>
2023-06-16 10:19:29 -0500

    261. addEventListener

--- domevents.js
+++ domevents.js
@@ -20,3 +20,27 @@ btnV3.onmouseenter = Hoverv3;
 document.querySelector('h1').onclick = () => {
     alert("You Clicked H1")
 }
+
+// 261. addEventListener
+
+const btnV4 = document.querySelector("#btnV4")
+
+btnV4.addEventListener('click', function () {
+    alert("You clicked V4.")
+})
+
+const btnV5 = document.querySelector("#btnV5")
+
+// btnV5.addEventListener('onclick', () => {
+//     alert("You clicked V5.")
+// })
+
+// btnV5.addEventListener('dblclick', () => {
+//     alert("You clicked V5.")
+// })
+
+btnV5.addEventListener('mouseup', () => {
+    alert("You clicked V5.")
+})
+
+// addEventListener allows multiple functions to add to a element whereas onclick doesn't.
