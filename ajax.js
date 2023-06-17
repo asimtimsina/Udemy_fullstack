64632eee237d3014216a0295998c0f6e26bcd6d2
asimtimsina <timsinaasim@gmail.com>
2023-06-17 17:27:59 -0500

    296. Making XHRs

--- ajax.js
+++ ajax.js
@@ -7,12 +7,35 @@ const dog = {
     owner: undefined
 }
 
-const jsondog = JSON.stringify(dog)
-console.log(jsondog)
+// const jsondog = JSON.stringify(dog)
+// console.log(jsondog)
 
-const dogdata = JSON.parse(jsondog)
-console.log(dogdata)
+// const dogdata = JSON.parse(jsondog)
+// console.log(dogdata)
 
 
 // 291. Using Hoppscotch (or Postman)
 // https://hoppscotch.io/
+
+
+
+// 296. Making XHRs
+
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+    console.log("Loaded")
+    console.log(this.responseText)
+    const data = JSON.parse(this.responseText)
+    console.log('name:', data.name)
+}
+
+req.onerror = function () {
+    console.log("Error")
+    console.log(this)
+}
+
+req.open("GET", "https://swapi.dev/api/people/1");
+req.send();
+
