bc92684543bb91946ef268a4d50520125ef916bd
asimtimsina <timsinaasim@gmail.com>
2023-06-20 13:39:07 -0500

    306. JavaScript Classes

--- oop.js
+++ oop.js
@@ -60,34 +60,72 @@
 ///////////////////////
 // 305. Constructor Functions
 
-function Color(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+// function Color(r, g, b) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// }
+
+// //define functions outside the constructor
+// //cannot use arrow function
+
+// Color.prototype.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r},${g},${b})`
+// }
+
+// Color.prototype.hex = function () {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// Color.prototype.rgba = function (a = "1.0") {
+//     const { r, g, b } = this;
+//     return `rgba(${r},${g},${b},${a})`
+// }
+
+// const color1 = new Color(20, 155, 24);
+// const color2 = new Color(202, 155, 24);
+
+// console.log(color1.rgb === color2.rgb) // returns true
+// console.log(color1.rgba()) // returns true
+// console.log(color1.rgba("0.5")) // returns true
+// document.body.style.backgroundColor = color1.rgba();
+
+///////////////////////////////////
+//306. JavaScript Classes
+
+class Color {
+    constructor(r, g, b, name = 'None') {
+        //runs immediately
+        this.r = r
+        this.g = g
+        this.b = b
+        this.name = name;
+    }
+
+    greet() {
+
+        return `Hello from Color ${this.name}`
+    }
+
+    rgb(r, g, b) {
+
+        return `rgb(${this.r},${this.g},${this.b} )`
+    }
+
+    hex = function () {
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+
+    rgba = function (a = 1.0) {
+        return `rgba(${this.r},${this.g},${this.b},${a} )`
+
+    }
+
 }
 
-//define functions outside the constructor
-//cannot use arrow function
 
-Color.prototype.rgb = function () {
-    const { r, g, b } = this;
-    return `rgb(${r},${g},${b})`
-}
-
-Color.prototype.hex = function () {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-Color.prototype.rgba = function (a = "1.0") {
-    const { r, g, b } = this;
-    return `rgba(${r},${g},${b},${a})`
-}
-
-const color1 = new Color(20, 155, 24);
-const color2 = new Color(202, 155, 24);
-
-console.log(color1.rgb === color2.rgb) // returns true
-console.log(color1.rgba()) // returns true
-console.log(color1.rgba("0.5")) // returns true
-document.body.style.backgroundColor = color1.rgba();
+const c1 = new Color(233, 111, 222, 'tomato');
+const white = new Color(255, 255, 255, 'White');
