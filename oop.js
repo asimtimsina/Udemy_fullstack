ceb0fd9f3b8accc738006981bc50434f19be57f6
asimtimsina <timsinaasim@gmail.com>
2023-06-20 10:46:37 -0500

    304. Factory Functions

--- oop.js
+++ oop.js
@@ -1,21 +1,58 @@
-console.log("HII")
+// console.log("HII")
 
-// 302. What On Earth Are Prototypes
+// // 302. What On Earth Are Prototypes
 
-String.prototype.yell = function () {
-    return (`OMG!!! ${this.toString().toUpperCase()}!!!`)
+// String.prototype.yell = function () {
+//     return (`OMG!!! ${this.toString().toUpperCase()}!!!`)
+// }
+
+
+// const test = "asimo"
+// test.yell();
+
+
+// Array.prototype.pop = function () {
+//     return 'I need this element.'
+// }
+
+// console.log([1, 2, 3].pop())
+
+// // prototype -> actual prototype
+// // _proto -> reference to the prototype
+
+
+////////////////////////////////
+// 303. Intro to Object Oriented Programming
+
+// 304. Factory Functions
+
+function hex(r, g, b) {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-
-const test = "asimo"
-test.yell();
-
-
-Array.prototype.pop = function () {
-    return 'I need this element.'
+function rgb(r, g, b) {
+    return `rgb(${r},${g},${b} )`
 }
 
-console.log([1, 2, 3].pop())
+hex(255, 100, 25); //'#ff6419'
 
-// prototype -> actual prototype
-// _proto -> reference to the prototype
+function makeColor(r, g, b) {
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function () {
+        const { r, g, b } = this;
+        return `rgb(${r},${g},${b})`
+    }
+
+    color.hex = function () {
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return color;
+}
+
+const firstColor = makeColor(35, 355, 150);
+firstColor.rgb()
+firstColor.hex()
