484285f2702895b5094c3433f742bdee559eba30
asimtimsina <timsinaasim@gmail.com>
2023-06-22 12:29:23 -0500

    342. The Request & Response Objects

--- firstExpressApp/index.js
+++ firstExpressApp/index.js
@@ -1,15 +1,31 @@
+// 341. Our Very First Express App
+
+
 const express = require('express');
 
 const app = express()
-console.log(app)
+// console.log(app)
 
 
-app.use(() => {
+// app.use(() => {
+//     console.log("All request come here.")
+// })
+
+// app.listen(3000, () => {
+//     console.log("Listening in port 3000")
+// })
+
+
+// 342. The Request & Response Objects
+
+app.use((req, res) => {
     console.log("All request come here.")
+    // console.dir(req)
+    // res.send("Hello, we got your response!")
+    // res.send({ color: "red" })
+    res.send("<h1> Hello from Express.js </h1>")
 })
 
 app.listen(3000, () => {
     console.log("Listening in port 3000")
 })
-
-
