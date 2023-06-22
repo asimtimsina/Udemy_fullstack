0fc42cc9715a835c53a4381b569b81ba3b067ac9
asimtimsina <timsinaasim@gmail.com>
2023-06-22 12:40:52 -0500

    343. Express Routing Basics

--- firstExpressApp/index.js
+++ firstExpressApp/index.js
@@ -18,14 +18,51 @@ const app = express()
 
 // 342. The Request & Response Objects
 
+// app.use((req, res) => {
+//     console.log("All request come here.")
+//     // console.dir(req)
+//     // res.send("Hello, we got your response!")
+//     // res.send({ color: "red" })
+//     res.send("<h1> Hello from Express.js </h1>")
+// })
+
+// app.listen(3000, () => {
+//     console.log("Listening in port 3000")
+// })
+
+
+// 343. Express Routing Basics
+
+app.get('/', (req, res) => {
+    res.send('<h1>Youre at Homepage</h1 > ')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('Meow')
+})
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page Not Found</h1 > ')
+})
+
+app.post('/cats', (req, res) => {
+    res.send('This is a post request to Cats. THis is different that GET requestpos')
+})
+
+
+app.get('/dogs', (req, res) => {
+    res.send('Woof')
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening in port 3000")
+})
+
 app.use((req, res) => {
     console.log("All request come here.")
     // console.dir(req)
     // res.send("Hello, we got your response!")
     // res.send({ color: "red" })
     res.send("<h1> Hello from Express.js </h1>")
-})
-
-app.listen(3000, () => {
-    console.log("Listening in port 3000")
 })
