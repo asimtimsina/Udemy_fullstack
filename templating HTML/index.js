877a79a4163b8708a528f8316a00dea07f15604c
asimtimsina <timsinaasim@gmail.com>
2023-06-23 10:25:09 -0500

    350. Setting The Views Directory

--- templating HTML/index.js
+++ templating HTML/index.js
@@ -4,6 +4,11 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
+
+// 350. Setting The Views Directory
+const path = require('path');
+app.set('views', path.join(__dirname, '/views'));
+
 app.get('/', (req, res) => {
     res.render('home')
 })
