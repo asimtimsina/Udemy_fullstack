0ffdddf33d07518b4eae0fe84d7c0389f9128bab
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-01 12:27:09 -0500

    394. Insert Many

--- moongose/index.js
+++ moongose/index.js
@@ -36,3 +36,16 @@ const Herapheri_2 = new Movie({ title: 'Hera pheri 2', year: 2010, score: 7, rat
 
 
 
+// 394. Insert Many
+
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+    .then(data => {
+        console.log('It worked!');
+        console.log(data);
+    })
