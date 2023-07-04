74442dba8f4996439bba0c4f847c066b122b1549
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-03 19:08:43 -0500

    430. More Middleware Practice

--- middleware_func/index.js
+++ middleware_func/index.js
@@ -6,17 +6,25 @@ const morgan = require('morgan');
 app.use(morgan('tiny'))
 
 // 429. Defining Our Own Middleware
+// app.use((req, res, next) => {
+//     console.log("This is the first middleware");
+//     next();
+//     console.log("This is the first middleware - after calling next");
+
+// })
+
+// app.use((req, res, next) => {
+//     console.log("This is the second middleware");
+//     next();
+// })
+
+// 430. More Middleware Practice
 app.use((req, res, next) => {
-    console.log("This is the first middleware");
+    console.log(req.method.toUpperCase(), req.path);
     next();
-    console.log("This is the first middleware - after calling next");
 
 })
 
-app.use((req, res, next) => {
-    console.log("This is the second middleware");
-    next();
-})
 
 app.get('/', (req, res) => {
     res.send('HOME!')
