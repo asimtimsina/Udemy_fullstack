e76b0917945fb0d72a3ffcdcad40750ddb92dbfa
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-07 10:58:54 -0500

    512. Auth Demo: Registering

--- Auth_demo/index.js
+++ Auth_demo/index.js
@@ -5,18 +5,54 @@ const app = express();
 
 const User = require('./models/user');
 
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://127.0.0.1:27017/authdemo')
+    .then(() => {
+        console.log("Mongo Coonection open")
+    })
+    .catch(() => {
+        console.log("Mongo Error")
+    })
 
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+
+    // res.redirect('/secret')
+
+    res.send('This is the homepage');
+})
 
 app.get('/register', (req, res) => {
+
+    // res.redirect('/secret')
+
     res.render('register');
 })
 
 
-app.post('/register', (req, res) => {
-    res.render('register');
+app.post('/register', async (req, res) => {
+    // console.log(req.body);
+    const { user } = req.body;
+    const hashedPw = await bcrypt.hash(user.password, 12);
+
+    const newUser = new User({
+        username: user.username,
+        password: hashedPw
+    })
+    await newUser.save();
+
+
+
+    res.redirect('/');
+
+    // res.send(req.body)
+    // res.render('register');
 })
 
 
