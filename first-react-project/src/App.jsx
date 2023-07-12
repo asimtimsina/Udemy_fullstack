75437af4c6d662b97f81909778cc1fcb85b47acc
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-12 16:57:57 -0500

    615. The React Developer Tools

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -5,7 +5,7 @@
 
 // import Greeter from './Greeter'
 // import Die from './Die'
-// import ListPicker from "../ListPicker"
+import ListPicker from "../ListPicker"
 import DoubleDice from "./DoubleDice"
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       {/* <Greeter person='Asim' from='Denton' />
       <Greeter person='Nick' from='Asim' /> */}
 
-      {/* <ListPicker values={['Asim', 'Nick', 'Bish']} /> */}
+      <ListPicker values={['Asim', 'Nick', 'Bish']} />
 
       <DoubleDice />
       <DoubleDice />
