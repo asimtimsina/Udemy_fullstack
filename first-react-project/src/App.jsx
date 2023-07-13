bbc5a324ff8f1d22a919f75eca747eaa2ad10021
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-12 19:56:26 -0500

    624. Configuring ESLint

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -25,11 +25,14 @@ function App() {
   ]
 
   const properties = [
-    { id: 123100, name: 'Desert Yurt', rating: 4.9, price: 150 },
-    { id: 123101, name: 'Lust Mountain', rating: 4.8, price: 125 },
-    { id: 123102, name: 'OceanView Condo', rating: 4.1, price: 320 },
-    { id: 123103, name: 'Gold Miner Campground', rating: 4.5, price: 250 },
-  ]
+    { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+    { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+    { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+    { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+    { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+    { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+  ];
+
 
   return (
     <>
