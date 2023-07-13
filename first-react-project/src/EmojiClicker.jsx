fe68694455a0d7b887a4e90db3be319497c58693
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-13 13:12:47 -0500

    643. Deleting From Arrays The React Way

--- first-react-project/src/EmojiClicker.jsx
+++ first-react-project/src/EmojiClicker.jsx
@@ -3,19 +3,23 @@ import { v4 as uuid } from 'uuid';
 
 export default function EmojiClicker() {
     const [emojis, setEmoji] = useState([{ id: uuid(), emoji: "😂" }])
-
-
     const addEmoji = () => {
         // setEmoji([...emojis, "😂"])
         setEmoji(old => { return [...old, { id: uuid(), emoji: "😂" }] })
     }
-
-
+    const deleteEmoji = (id) => {
+        //delete emoji with id
+        // console.log(id)
+        // const newList = emojis.filter(e => e.id !== id);
+        // console.log(newList);
+        setEmoji(previousEmojis => {
+            return previousEmojis.filter(e => e.id !== id);
+        })
+    }
 
     return (
-
         <div>
-            {emojis.map(e => <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>)}
+            {emojis.map(e => <span key={e.id} onClick={() => deleteEmoji(e.id)} style={{ fontSize: "4rem" }}>{e.emoji}</span>)}
             <br /> <button onClick={addEmoji}>Add Emoji </button>
         </div>
     )
