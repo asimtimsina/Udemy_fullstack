01b478aca46b99321cbcd933be2b3b100e442e99
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-15 17:46:56 -0500

    667. useEffect Dependencies

--- first-react-project/src/CounterNew.jsx
+++ first-react-project/src/CounterNew.jsx
@@ -3,17 +3,18 @@ import { useState, useEffect } from "react";
 
 export default function CounterNew() {
 
-
+    const [name, setName] = useState("");
     const [count, setCount] = useState(0);
     useEffect(function myEffect() {
         console.log("My Effect was called")
-    })
+    }, [count])
 
     const increment = () => {
-
         setCount(c => c + 1);
+    }
 
-
+    const handleChange = (evt) => {
+        setName(evt.target.value)
     }
 
 
@@ -23,7 +24,12 @@ export default function CounterNew() {
 
             <h1>{count}</h1>
             <button onClick={increment}>+1</button>
+            <br />
+            <label htmlFor="name">Name: {name}</label>
+            <input type="text" onChange={handleChange} name="name" id="name" value={name} />
+
         </div>
+
 
 
     )
