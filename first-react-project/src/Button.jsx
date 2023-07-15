0f918633e4206f4eedbb420b219d4c1aa9c69f73
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-15 09:54:31 -0500

    653. Passing Functions That Update State

---
+++ first-react-project/src/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css'
+
+
+
+export default function Button({ clickFunc, label = "Click Me" }) {
+
+    return (
+        <button className='Button' onClick={clickFunc}>{label}</button>
+    )
+
+
+}
--- first-react-project/src/LuckyN.jsx
+++ first-react-project/src/LuckyN.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { d6, sum, getRolls } from "./utils"
 import Dice from "./Dice";
+import Button from "./Button";
+
 
 function LuckyN({ numDice = 2, winCheck }) {
 
@@ -19,8 +21,8 @@ function LuckyN({ numDice = 2, winCheck }) {
 
             <Dice dice={dice} />
 
-            <button onClick={reRollFunc}>Re-roll Dice</button>
-
+            {/* <button onClick={reRollFunc}>Re-roll Dice</button> */}
+            <Button clickFunc={reRollFunc} label="Re Roll" />
         </main>
 
     )
