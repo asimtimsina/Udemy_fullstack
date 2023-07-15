ccf1c5a857ce63e07208f85eae5101c8b843b989
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-15 15:29:08 -0500

    664. Validations using React Hook Form

--- first-react-project/src/AutoValidatedShoppingList.jsx
+++ first-react-project/src/AutoValidatedShoppingList.jsx
@@ -7,12 +7,25 @@ export default function ShoppingListForm({ add_func }) {
     const [shoppingForm, setShoppingForm] = useState({ name: "", quantity: 0 })
 
 
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm({ mode: "onChange" })
 
     const handleChange = (evt) => {
         setShoppingForm(currData => {
             return { ...currData, [evt.target.name]: evt.target.value }
         })
+    }
+
+    const registerOptions = {
+        name: { required: "Name cannot be blank" },
+        quantity: {
+            required: "Quantity cannot be blank.",
+            min: { value: 0, message: "Quanitity cannot be less than 0." },
+            max: { value: 100, message: "Quanitity cannot be greater than 100." }
+        },
+        password: {
+            required: "Password cannot be blank",
+            minLength: { value: 6, message: "Password must be more than 6 character long." }
+        }
     }
 
     const check = (data) => {
@@ -25,12 +38,18 @@ export default function ShoppingListForm({ add_func }) {
 
             <label htmlFor="name">Product Name: </label>
             {/* <input onChange={handleChange} type="text" id='name' name="name" placeholder="name" value={shoppingForm.name} /> */}
-            <input {...register("name", { required: "Product name is required" })} />
-            <p>{errors.firstName?.message}</p>
+            <input {...register("name", registerOptions.name)} />
+            <p>{errors.name && errors.name.message}</p>
 
             <label htmlFor="quantity">Quantity: </label>
             {/* <input onChange={handleChange} type="number" id='quantity' name="quantity" placeholder="quantity" value={shoppingForm.quantity} /> */}
-            <input {...register("quantity")} />
+            <input {...register("quantity", registerOptions.quantity)} />
+            <p>{errors.quantity && errors.quantity.message}</p>
+
+
+            <label htmlFor="password">Password</label>
+            <input type='password' {...register("password", registerOptions.password)} />
+            <p>{errors.password && errors.password.message}</p>
 
             <input type="submit" />
             {/* <button >Submit</button> */}
