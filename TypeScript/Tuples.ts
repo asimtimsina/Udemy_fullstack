9a8a7d7086541fa9ed4e187725fbd0a60342c4b6
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-27 11:42:11 -0500

    TS Tuples

---
+++ TypeScript/Tuples.ts
@@ -0,0 +1,28 @@
+// const tuser: (string|number)[] = [1,"at"]
+
+
+let tuser: [string, number, boolean]
+
+// tuser = [true, 1223,'at']; // not valid order
+tuser = ["at", 1223, true]; // valid order
+
+let rgb: [number, number, number] = [255, 255, 255];
+
+
+
+
+
+
+const User: [number, string]
+
+
+
+const newUser: User = [112, 'Asimo'];
+
+newUser[0] = 555;
+
+newUser[2] = true; // can add the third value cause only first 2 elemets are defined.
+
+
+
+export { }
