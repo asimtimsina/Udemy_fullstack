284baa04ba4cd958f1070d382188f737455c3aab
asimtimsina <79533233+asimtimsina@users.noreply.github.com>
2023-07-27 13:02:30 -0500

    TS Interface and Inheritance

--- TypeScript/MyInterface.ts
+++ TypeScript/MyInterface.ts
@@ -11,9 +11,36 @@ interface User {
     getCoupon(couponname: string, value: number): number
 }
 
+
+//can add to the existing interface
+interface User {
+    githubToken: string
+}
+
+
+//TS interface also supports inheritance
+
+interface Admin extends User {
+    role: "admin" | "TA" | "Learner"
+}
+
 const Asim: User = {
     dbId: 22,
     email: "a@a.com",
+    userId: 2211,
+    startTrail: () => {
+        return "Trail Started"
+    },
+    getCoupon: (name: "asim10", off: 10) => {
+        return 10
+    }
+
+}
+
+const Asimo: Admin = {
+    dbId: 22,
+    email: "a@a.com",
+    role: 'admin',
     userId: 2211,
     startTrail: () => {
         return "Trail Started"
